477ed6360f825510f618b3534731adaa92230e08
RNtaate <royhyde14@gmail.com>
2020-08-11 13:24:49 +0300

    Add get_content method to FetchContent class

--- lib/fetch_content.rb
+++ lib/fetch_content.rb
@@ -2,6 +2,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-class Fetch_content
-
+class FetchContent
+  def self.get_content(main_link, *selectors)
+    doc = Nokogiri::HTML(URI.open(main_link))
+    list = doc.css(*selectors)
+    list
+  end
 end
