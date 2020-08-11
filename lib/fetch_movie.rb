a1c57c184424acb87baf5e0d6b4b2aa5bf2b7133
RNtaate <royhyde14@gmail.com>
2020-08-11 17:53:41 +0300

    Add get_movie_content to Movie class

--- lib/fetch_movie.rb
+++ lib/fetch_movie.rb
@@ -5,4 +5,9 @@ class Movie
   def initialize(link)
     @doc = Nokogiri::HTML(URI.open(link))
   end
+
+  def get_movie_content(*selector)
+    list = @doc.css(*selector)
+    return list
+  end
 end
