d63a849cdb92b1ecd1ddd2209ef0e5ff10f0b892
RNtaate <royhyde14@gmail.com>
2020-08-11 12:43:18 +0300

    Add welcome method to Scraper class in main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
-puts 'Hello World'
+
+class Scraper
+
+  def welcome
+    puts "
+                        Welcome to NORP FLIX
+                        ********************
+        A place of information about the available NETFLIX movies
+        
+      Simply select a category from the category list, choose a movie 
+                from the provided list of that category and 
+                      receive information about it
+    ====================================================================
+    
+    "
+  end
+end
+
+sm = Scraper.new
+
+sm.welcome
