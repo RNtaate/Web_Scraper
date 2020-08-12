bfc35f12a2ff15f84f2ec3cac2b389400c877073
RNtaate <royhyde14@gmail.com>
2020-08-12 16:15:43 +0300

    Add rspec test case for display_movie_content method of Movie Class

--- spec/fetch_movie_spec.rb
+++ spec/fetch_movie_spec.rb
@@ -10,10 +10,19 @@ describe Movie do
   end
 
   describe "#get_movie_content" do
-    context "When given css selector strings a arguments" do
-      it 'Returns a string containing the content of all elements of a NodeSet' do
+    context "When given css selector strings as arguments" do
+      it 'Returns a string containing the contents of all elements of a NodeSet' do
         movie = Movie.new(web_link)
         expect(movie.get_movie_content('h1')).to eql("NORP ELECTRONICS")
+      end
+    end
+  end
+
+  describe '#display_movie_content' do
+    context "When given a webpage link string as an argument" do
+      it "Returns a string containing contents of elements from various NodeSets" do
+        movie = Movie.new
+        expect(movie.display_movie_content(web_link)).to be_a String
       end
     end
   end
