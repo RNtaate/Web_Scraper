ce8c22982185fcb4e7534096d6fdeb1246f3ad83
RNtaate <royhyde14@gmail.com>
2020-08-12 12:50:09 +0300

    Add rspec test cases for get_content method of FetchContent class

--- spec/fetch_content_spec.rb
+++ spec/fetch_content_spec.rb
@@ -1,1 +1,15 @@
+require 'rspec'
+require 'nokogiri'
 require './lib/fetch_content.rb'
+
+describe FetchContent do
+  let(:web_link){'https://rawcdn.githack.com/RNtaate/Sound-Electronics-store/ccadeae6e3e1eda77681a48f5f835e974dddcbd5/index.html'}
+
+  describe ".get_content" do
+
+    it "Returns a NodeSet when provided a webpage link string and css node selectors as arguments" do
+      expect(FetchContent.get_content(web_link, 'h1')[0].content).to eql("NORP ELECTRONICS")
+    end
+
+  end
+end
