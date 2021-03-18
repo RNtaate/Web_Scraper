43f3d95b4ec916526cf7af9e56907a67782e15b3
RNtaate <royhyde14@gmail.com>
2021-03-18 20:22:42 +0300

    Update CATEGORY_NAME in fetch_messages.rb file

--- lib/fetch_messages.rb
+++ lib/fetch_messages.rb
@@ -1,6 +1,6 @@
 module FetchMessages
   MAIN_LINK = 'https://www.netflix.com/ug/browse/genre/34399'.freeze
-  CATEGORY_NAME = 'h1.nm-collections-row-name'.freeze
+  CATEGORY_NAME = 'h2.nm-collections-row-name'.freeze
   CATEGORY_SECTION = 'ul.nm-content-horizontal-row-item-container'.freeze
   MOVIE_TITLE = 'h1.title-title'.freeze
   MOVIE_OVERVIEW = 'div.title-info-synopsis'.freeze
